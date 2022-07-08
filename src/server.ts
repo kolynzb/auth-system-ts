580926811d076cd6e94db83a1b9e113cbabcb48e
kolynzb <collinsbenda360@gmail.com>
2022-07-08 13:07:10 +0300

    Add Dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log("Test");
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv-safe";
+
+dotenv.config();
+
+const app = express();
+app.use(morgan("dev")); // logger
+
+app.get("/", async (req, res) => {
+  res.json({ hello: "world" });
+});
+
+const port = Number(process.env.PORT ?? 8080);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
